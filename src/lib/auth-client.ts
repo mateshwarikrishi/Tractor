8157e539680e24d9894ffe50e7c475b99963fe8c
jeakansh <eakanshjain22@gmail.com>
2026-05-22 16:21:44 +0530

    login fixed

--- src/lib/auth-client.ts
+++ src/lib/auth-client.ts
@@ -1,6 +1,11 @@
 "use client";
 import { createAuthClient } from "better-auth/react";
 
-export const authClient = createAuthClient();
+export const authClient = createAuthClient({
+  baseURL:
+    typeof window !== "undefined"
+      ? window.location.origin
+      : process.env.BETTER_AUTH_URL,
+});
 
 export const { signIn, signUp, signOut, useSession } = authClient;
